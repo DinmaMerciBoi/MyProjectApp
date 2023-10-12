ab1e50e9bd61336f92b4e723f9f27ff7a8fffa44
Dinma <dimmalives@gmail.com>
2023-10-12 15:07:41 -0230

    Terraform Scripts updated

--- Terraform Scripts/Ansible_Server/security.tf
+++ Terraform Scripts/Ansible_Server/security.tf
@@ -2,27 +2,22 @@
 resource "aws_security_group" "web-traffic" {
   name = "My_Security_Group2"
 
-  dynamic "ingress" {
-    iterator = port
-    for_each = var.ingressrules
-    content {
-      from_port   = port.value
-      to_port     = port.value
-      protocol    = "TCP"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  dynamic "egress" {
-    iterator = port
-    for_each = var.egressrules
-    content {
-      from_port   = port.value
-      to_port     = port.value
-      protocol    = "TCP"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
 
   tags = {
     "Name" = "My_SG2"
+  }
+}
--- Terraform Scripts/Jenkins_Server/security.tf
+++ Terraform Scripts/Jenkins_Server/security.tf
@@ -2,27 +2,20 @@
 resource "aws_security_group" "web-traffic" {
   name = "My_Security_Group1"
 
-  dynamic "ingress" {
-    iterator = port
-    for_each = var.ingressrules
-    content {
-      from_port   = port.value
-      to_port     = port.value
-      protocol    = "TCP"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  dynamic "egress" {
-    iterator = port
-    for_each = var.egressrules
-    content {
-      from_port   = port.value
-      to_port     = port.value
-      protocol    = "TCP"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
 
   tags = {
     "Name" = "My_SG1"
